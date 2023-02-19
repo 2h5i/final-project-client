a23ae67b0ae69d7f4e5e7ec9dadf97d2c3848b09
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-02-19 16:49:33 +0900

    [feat] 클라이언트 - 비밀번호 변경

--- js/update-user.js
+++ js/update-user.js
@@ -4,13 +4,11 @@
       method: 'PUT',
       timeout: 0,
       "headers": {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYTU2NzgiLCJhdXRoIjoiVVNFUiIsImV4cCI6MTY3Njc5MTAxMiwiaWF0IjoxNjc2Nzg3NDEyfQ._zJBdL1Hy0pkvuEXPEd6qKuPxn7RqUrrMoQxjJxHhR0",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYWRhMTIzNCIsImF1dGgiOiJVU0VSIiwiZXhwIjoxNjc2Nzk2MDM4LCJpYXQiOjE2NzY3OTI0Mzh9.EWz6nbXpwjIdU55HKm-bAuTuIt6pyOPl1n8da5gtJ7o",
         'Content-Type': 'application/json'
       },
       "data":JSON.stringify({
-        "userId":$('#userId').val(),
         "password":$('#userPassword').val(),
-        "email":$('#userEmail').val()
       }),
     }
     $.ajax(settings).done(function (response) {
@@ -19,3 +17,25 @@
         window.location.href="login.html";
     });
 };
+
+function isSame() {
+    let pw = document.getElementById("userPassword");
+    let confirmPW = document.getElementById("userPassword2");
+    if (pw.length < 8 || pw.length > 15) {
+        window.alert('비밀번호는 6글자 이상, 16글자 이하만 이용 가능합니다.');
+        pw.value=confirmPW.value='';
+        document.getElementById('same').innerHTML='';
+    }
+    if(pw.value!='' && confirmPW.value!='') {
+        if(pw.value==confirmPW.value) {
+            document.getElementById('same').innerHTML='비밀번호가 일치합니다.';
+            document.getElementById('same').style.color='green';
+            document.getElementById('submit-button').disabled = false;
+        }
+        else {
+            document.getElementById('same').innerHTML='비밀번호가 일치하지 않습니다.';
+            document.getElementById('same').style.color='red';
+            document.getElementById('submit-button').disabled = true;
+        }
+    }
+}
