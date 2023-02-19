114b6992c12d322c37c0e46950c3c0569f88006a
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2023-02-19 15:30:07 +0900

    [feat] 클라 - 회원정보수정 기능

--- js/update-user.js
+++ js/update-user.js
@@ -1,20 +1,21 @@
   function updateUser(){
     var settings = {
-      url: 'http://localhost:8080/api/users/23',
+      url: 'http://localhost:8080/api/users/25',
       method: 'PUT',
       timeout: 0,
       "headers": {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyYWEiLCJhdXRoIjoiVVNFUiIsImV4cCI6MTY3Njc3NzMwNywiaWF0IjoxNjc2NzczNzA3fQ.5D4wK-8QkzIv17v4jSfsXj4UO3R2dFZ3NDMSObAAngw"
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYTU2NzgiLCJhdXRoIjoiVVNFUiIsImV4cCI6MTY3Njc5MTAxMiwiaWF0IjoxNjc2Nzg3NDEyfQ._zJBdL1Hy0pkvuEXPEd6qKuPxn7RqUrrMoQxjJxHhR0",
+        'Content-Type': 'application/json'
       },
       "data":JSON.stringify({
         "userId":$('#userId').val(),
-        "userPassword":$('#userPassword').val(),
-        "userEmail":$('#userEmail').val()
+        "password":$('#userPassword').val(),
+        "email":$('#userEmail').val()
       }),
     }
     $.ajax(settings).done(function (response) {
         console.log(response)
-        alert("변경되었습니다");
-        window.location.href="mypage.html";
+        alert("변경되었습니다, 다시 로그인해주세요");
+        window.location.href="login.html";
     });
 };
