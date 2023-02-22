79b3863d2bdce671eaad7f6c3f633aef670cdbde
dongnobi97 <117060963+dongnobi97@users.noreply.github.com>
2023-02-22 11:57:24 +0900

    [feat] 클라 - 회원가입 구현
    [feat] 클라 - 회원가입 구현

--- js/signup.js
+++ js/signup.js
@@ -25,7 +25,7 @@ const errMsg = {
   const idErrorMsgEl = document.querySelector('#info__id .error-msg')
   const idCheckBtn = document.querySelector('#id-check')
   idInputEl.addEventListener('change', () => {
-    const idRegExp = /^[a-zA-Z0-9]{6,20}$/
+    const idRegExp = /^[a-zA-Z0-9]{4,10}$/
     if(idRegExp.test(idInputEl.value)) { // 정규식 조건 만족 O
       idErrorMsgEl.textContent = ""
       account.id = idInputEl.value
@@ -37,19 +37,7 @@ const errMsg = {
     console.log(account)
   });
   
-  idCheckBtn.addEventListener('click', () => {
-    const randVal = Math.floor(Math.random() * 10)
-    if(account.id !== null) {
-      if(randVal < 7) {
-        idErrorMsgEl.style.color = "green"
-        idErrorMsgEl.textContent = errMsg.id.success
-      }
-      else {
-        idErrorMsgEl.style.color = "red"
-        idErrorMsgEl.textContent = errMsg.id.fail
-      }
-    }
-  })
+
   
   /*** SECTION - PASSWORD ***/
   // pwVal: 패스워드, pwReVal: 패스워드 재입력, isPwValid: 패스워드 유효 여부
@@ -75,7 +63,7 @@ const errMsg = {
   const pwInputEl = document.querySelector('#info__pw input')
   const pwErrorMsgEl = document.querySelector('#info__pw .error-msg')
   pwInputEl.addEventListener('change', () => {
-    const pwRegExp = /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/
+    const pwRegExp = /^[A-Za-z0-9]{8,15}$/;
     pwVal = pwInputEl.value
     if(pwRegExp.test(pwVal)) { // 유효성 검사 성공
       isPwValid = true
@@ -98,73 +86,26 @@ const errMsg = {
     console.log(pwVal, pwReVal, isPwValid, account)
   });
   
-  /*** SECTION - EMAIL ***/
-  emailList = ["", ""]
-  function checkEmailValid() {
-    if(emailList[0] !== "" && emailList[1] !== "") {
-      account.email = emailList.join('@')
-    }
-    else
-    account.email = null
-  }
+ 
   
-  const emailInputEl = document.querySelector('#email-txt')
-  emailInputEl.addEventListener('change', () => {
-    emailList[0] = emailInputEl.value
-    checkEmailValid()
-    console.log(account, emailList)
-  })
-  
-  const domainInputEl = document.querySelector('#domain-txt')
-  domainInputEl.addEventListener('change', () => {
-    emailList[1] = domainInputEl.value
-    checkEmailValid()
-    console.log(account, emailList)
-  })
-  
-  // 도메인 직접 입력 or domain option 선택
-  const domainListEl = document.querySelector('#domain-list')
-  domainListEl.addEventListener('change', () => {
-    // option에 있는 도메인 선택 시
-    const domainSelected = domainListEl.value
-    if(domainSelected !== "type") {
-      // 선택한 도메인을 input에 입력하고 disabled
-      domainInputEl.value = domainSelected
-      domainInputEl.disabled = true
-      emailList[1] = domainSelected
-    } else { // 직접 입력
-      // input 내용 초기화 & 입력 가능하도록 변경
-      domainInputEl.value = ""
-      domainInputEl.disabled = false
-      emailList[1] = ""
+
+  function signupUser(){
+    var settings = {
+      url: 'http://localhost:8080/api/auth/signup',
+      method: 'POST',
+      timeout: 0,
+      "headers": {
+        'Content-Type': 'application/json',
+      },
+      "data":JSON.stringify({
+        "userId":$('#userName').val(),
+        "password":$('#userPassword').val(),
+        "email":$('#email-txt').val(),
+      }),
     }
-    checkEmailValid()
-    console.log(account, emailList)
-  })
-  
-  /*** SUBMIT ***/
-  const submitBtn = document.querySelector('#submit')
-  const resultFailEl = document.querySelector('#result-fail')
-  submitBtn.addEventListener('click', function() {
-    let isAllFilled = true
-    const word = {  
-      id: "아이디를",
-      pw: "비밀번호를",
-      email: "이메일을",
-      birth: "생년월일을",
-      mobile: "휴대폰 번호를"
-    }
-    for(element in account) {
-      if(account[element] === null) {
-        resultFailEl.textContent = word[element] + " 다시 한번 확인해주세요"
-        isAllFilled = false
-        break
-      }
-    }
-    if (isAllFilled === true) {
-      resultFailEl.textContent = ""
-      setTimeout(function() {
-        alert("서버 전송 데이터 : " + JSON.stringify(account))
-      }, 300)
-    }
-  })
+    $.ajax(settings).done(function (response) {  
+        alert("회원가입이 완료되었습니다.");
+        window.location.href="login.html"; 
+    });
+    
+};
